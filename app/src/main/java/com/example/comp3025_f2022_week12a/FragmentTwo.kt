f7a4bf33ac1e345a0654dbeaeeca654f91cd75e4
Raymund <71455069+RaymundRama@users.noreply.github.com>
2022-12-04 17:19:09 -0500

    fragment two

--- app/src/main/java/com/example/comp3025_f2022_week12a/FragmentTwo.kt
+++ app/src/main/java/com/example/comp3025_f2022_week12a/FragmentTwo.kt
@@ -3,4 +3,17 @@ package com.example.comp3025_f2022_week12a
 import androidx.fragment.app.Fragment
 
 class FragmentTwo: Fragment(R.layout.fragment_two) {
+
+    override fun onViewCreate(view: View, savedInstanceState: Bundle?)
+    {
+        super.onViewCreated(view, savedInstanceState)
+
+        var FragmentTwoToOneButton = getView()?.findViewById<Button>(R.id.fragment_two_to_one_button)
+
+        FragmentTwoToOneButton?.setOnClickListener{
+            (activity as MainActivity).supportFragmentManager.commit{
+                replace<FragmentOne>(R.id.fragment_container)
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/comp3025_f2022_week12a/MainActivity.kt
+++ app/src/main/java/com/example/comp3025_f2022_week12a/MainActivity.kt
@@ -6,14 +6,16 @@ import android.text.TextUtils.replace
 import android.widget.Button
 import androidx.fragment.app.Fragment
 
-class MainActivity : AppCompatActivity() {
 
-    var FragmentOneButton = findViewById<Button>(R.id.fragment_one_button)
-    var FragmentTwoButton = findViewById<Button>(R.id.fragment_two_button)
+
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var FragmentOneButton = findViewById<Button>(R.id.fragment_one_button)
+        var FragmentTwoButton = findViewById<Button>(R.id.fragment_two_button)
 
         FragmentOneButton.setOnClickListener{
 
