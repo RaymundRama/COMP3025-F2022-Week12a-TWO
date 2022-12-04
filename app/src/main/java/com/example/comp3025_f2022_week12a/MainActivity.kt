be67b62a987cb5d9048d3f508ae1d6bc191b07df
Raymund <71455069+RaymundRama@users.noreply.github.com>
2022-12-04 15:05:11 -0500

    things are broken i'm restarting my application

--- app/src/main/java/com/example/comp3025_f2022_week12a/MainActivity.kt
+++ app/src/main/java/com/example/comp3025_f2022_week12a/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.comp3025_f2022_week12a
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils.replace
+import android.widget.Button
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
+
+    var FragmentOneButton = findViewById<Button>(R.id.fragment_one_button)
+    var FragmentTwoButton = findViewById<Button>(R.id.fragment_two_button)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        FragmentOneButton.setOnClickListener{
+
+        }
+
+        FragmentTwoButton.setOnclickListener{
+
+        }
+
     }
 }
